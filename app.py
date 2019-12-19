f0fffd123571da07b6e4653c2f12c6b09ef711e2
Ubuntu <ubuntu@ip-172-31-35-9.ec2.internal>
2019-12-19 14:37:21 +0000

    check env

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = os.environ["MONGO_DBNAME"]
+app.config["MONGO_URI"] = os.environ["MONGO_URI"]
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
