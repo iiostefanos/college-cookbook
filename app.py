7993d7582646b603558fed42413119904971eb53
Ubuntu <ubuntu@ip-172-31-35-9.ec2.internal>
2020-01-13 22:18:54 +0000

    testing edits

--- app.py
+++ app.py
@@ -7,15 +7,16 @@ if path.exists("env.py"):
     import env
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = os.environ["MONGO_DBNAME"]
+app.config["MONGO_DBNAME"] = os.environ["MONGODB_college_cookbook"]
 app.config["MONGO_URI"] = os.environ["MONGO_URI"]
 
 mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
