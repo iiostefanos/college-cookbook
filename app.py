616af8d4bb3bad359ea34b2c517620a6b8bcde3b
Ubuntu <ubuntu@ip-172-31-35-9.ec2.internal>
2020-03-02 19:53:29 +0000

    fixed errors on heroku with bcrypt line

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_pymongo import PyMongo
-import bcrypt
 from werkzeug.security import generate_password_hash, check_password_hash
 from bson.objectid import ObjectId 
 from os import path
